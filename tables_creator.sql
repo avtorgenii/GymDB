ba8ace30b700942983dfa65fa9e3e886ad028b78
hardwak <ymhere2@gmail.com>
2024-11-05 15:13:41 +0100

    added not nulls, enums, checks

--- tables_creator.sql
+++ tables_creator.sql
@@ -1,84 +1,75 @@
--- Users Table
 CREATE TABLE "User" (
     ID SERIAL PRIMARY KEY,
-    Email VARCHAR(100) UNIQUE,
+    Email VARCHAR(100) UNIQUE not null ,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
     Phone VARCHAR(15) UNIQUE,
-    Password VARCHAR(255),
-    RegistrationDate DATE,
-    IsActive VARCHAR(10)
+    Password VARCHAR(255) not null ,
+    RegistrationDate DATE DEFAULT current_date not null ,
+    IsActive BOOLEAN default true not null
 );
 
--- Administrator Table (References User.ID)
 CREATE TABLE "Administrator" (
     AdministratorID SERIAL PRIMARY KEY,
-    UserID INT,
+    UserID INT not null,
     FOREIGN KEY (UserID) REFERENCES "User"(ID) ON DELETE CASCADE
 );
 
--- Manager Table (References User.ID, AddedBy references User.ID)
 CREATE TABLE "Manager" (
     ManagerID SERIAL PRIMARY KEY,
-    UserID INT,
+    UserID INT not null,
     AddedBy INT,
     FOREIGN KEY (UserID) REFERENCES "User"(ID) ON DELETE CASCADE,
     FOREIGN KEY (AddedBy) REFERENCES "Administrator"(AdministratorID) ON DELETE SET NULL
 );
 
--- Technician Table (References User.ID, AddedBy references User.ID)
 CREATE TABLE "Technician" (
     TechnicianID SERIAL PRIMARY KEY,
-    UserID INT,
+    UserID INT not null ,
     AddedBy INT,
     FOREIGN KEY (UserID) REFERENCES "User"(ID) ON DELETE CASCADE,
-    FOREIGN KEY (AddedBy) REFERENCES "User"(ID) ON DELETE SET NULL
+    FOREIGN KEY (AddedBy) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
 );
 
--- Trainer Table (References User.ID, AddedBy references User.ID)
 CREATE TABLE "Trainer" (
     TrainerID SERIAL PRIMARY KEY,
-    UserID INT,
+    UserID INT not null,
     AddedBy INT,
     FOREIGN KEY (UserID) REFERENCES "User"(ID) ON DELETE CASCADE,
-    FOREIGN KEY (AddedBy) REFERENCES "User"(ID) ON DELETE SET NULL
+    FOREIGN KEY (AddedBy) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
 );
 
--- Client Table (References User.ID)
 CREATE TABLE "Client" (
     ClientID SERIAL PRIMARY KEY,
-    UserID INT,
+    UserID INT not null,
     FOREIGN KEY (UserID) REFERENCES "User"(ID) ON DELETE CASCADE
 );
 
--- Offer Table
 CREATE TABLE "Offer" (
     ID SERIAL PRIMARY KEY,
-    Name VARCHAR(50),
+    Name VARCHAR(50) not null,
     Description VARCHAR(255),
-    Price FLOAT,
-    Duration INT
+    Price FLOAT not null check ( Price >= 0.00 ),
+    Duration INT not null check ( Duration >= 0 ),
+    AvailableToPurchase BOOLEAN not null DEFAULT true
 );
 
--- Membership Table (References Client.ClientID, Offer.ID)
 CREATE TABLE "Membership" (
     ID SERIAL PRIMARY KEY,
-    StartDate DATE,
-    EndDate DATE,
-    OfferID INT,
-    OwnedBy INT,
+    StartDate DATE not null default current_date,
+    EndDate DATE CHECK ( EndDate >= StartDate ) not null,
+    OfferID INT not null,
+    OwnedBy INT not null,
     FOREIGN KEY (OfferID) REFERENCES "Offer"(ID) ON DELETE CASCADE,
     FOREIGN KEY (OwnedBy) REFERENCES "Client"(ClientID) ON DELETE CASCADE
 );
 
--- TrainingType Table
 CREATE TABLE "TrainingType" (
     ID SERIAL PRIMARY KEY,
-    Name VARCHAR(50),
+    Name VARCHAR(50) not null,
     Description VARCHAR(500)
 );
 
--- TrainerQualifications Table (References Trainer.TrainerID, TrainingType.ID)
 CREATE TABLE "TrainerQualifications" (
     TrainerID INT,
     TrainingTypeID INT,
@@ -87,47 +78,44 @@ CREATE TABLE "TrainerQualifications" (
     FOREIGN KEY (TrainingTypeID) REFERENCES "TrainingType"(ID) ON DELETE CASCADE
 );
 
--- Availability Table (References Trainer.TrainerID)
 CREATE TABLE "Availability" (
     ID SERIAL PRIMARY KEY,
-    TrainerID INT,
-    Date DATE,
-    StartTime TIME,
-    EndTime TIME,
+    TrainerID INT not null,
+    Date DATE not null default current_date,
+    StartTime TIME not null,
+    EndTime TIME CHECK ( EndTime > StartTime ) not null,
     FOREIGN KEY (TrainerID) REFERENCES "Trainer"(TrainerID) ON DELETE CASCADE
 );
 
--- DepartmentLocation Table (References Manager.ManagerID)
 CREATE TABLE "DepartmentLocation" (
     ID SERIAL PRIMARY KEY,
-    Name VARCHAR(255),
-    Adress VARCHAR(255),
+    Name VARCHAR(255) not null,
+    Adress VARCHAR(255) not null,
     ManagedBy INT,
     FOREIGN KEY (ManagedBy) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
 );
 
--- Hall Table (References DepartmentLocation.ID)
 CREATE TABLE "Hall" (
     ID SERIAL PRIMARY KEY,
-    DepartmentLocationID INT,
-    Name VARCHAR(255),
+    Name VARCHAR(255) not null,
+    DepartmentLocationID INT not null,
     FOREIGN KEY (DepartmentLocationID) REFERENCES "DepartmentLocation"(ID) ON DELETE CASCADE
 );
 
--- Equipment Table (References Hall.ID)
 CREATE TABLE "Equipment" (
     ID SERIAL PRIMARY KEY,
-    Name VARCHAR(255),
-    HallID INT,
+    Name VARCHAR(255) not null,
+    HallID INT not null,
     FOREIGN KEY (HallID) REFERENCES "Hall"(ID) ON DELETE CASCADE
 );
 
--- Fault Table (References Technician.TechnicianID, Manager.ManagerID, Equipment.ID)
+CREATE TYPE FAULT_STATUS AS ENUM('Reported','Under Repair','Fixed','Cannot Be Repaired');
+
 CREATE TABLE "Fault" (
     ID SERIAL PRIMARY KEY,
     Description VARCHAR(255),
-    Status VARCHAR(18),
-    EquipmentID INT,
+    Status FAULT_STATUS not null default 'Reported',
+    EquipmentID INT not null,
     HandledBy INT,
     AddedBy INT,
     FOREIGN KEY (EquipmentID) REFERENCES "Equipment"(ID) ON DELETE CASCADE,
@@ -135,46 +123,44 @@ CREATE TABLE "Fault" (
     FOREIGN KEY (AddedBy) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
 );
 
--- LockerRoom Table (References DepartmentLocation.ID)
 CREATE TABLE "LockerRoom" (
     ID SERIAL PRIMARY KEY,
-    Type VARCHAR(50),
-    DepartmentLocationID INT,
+    Type VARCHAR(50) not null,
+    DepartmentLocationID INT not null,
     FOREIGN KEY (DepartmentLocationID) REFERENCES "DepartmentLocation"(ID) ON DELETE CASCADE
 );
 
--- Locker Table (References LockerRoom.ID, OccupiedBy references Client.ClientID)
+CREATE TYPE LOCKER_STATUS AS ENUM('Free','Occupied','Not available');
+
 CREATE TABLE "Locker" (
     ID SERIAL PRIMARY KEY,
-    Number INT,
-    Status VARCHAR(13),
-    LockerRoomID INT,
+    Number INT not null,
+    Status LOCKER_STATUS not null default 'Free',
+    LockerRoomID INT not null,
     OccupiedBy INT,
     FOREIGN KEY (LockerRoomID) REFERENCES "LockerRoom"(ID) ON DELETE CASCADE,
     FOREIGN KEY (OccupiedBy) REFERENCES "Client"(ClientID) ON DELETE SET NULL
 );
 
--- Training Table (References Trainer.TrainerID, Manager.ManagerID, TrainingType.ID, Hall.ID)
 CREATE TABLE "Training" (
     ID SERIAL PRIMARY KEY,
-    Date DATE,
-    StartTime TIME,
-    EndTime TIME,
-    TrainingTypeID INT,
+    Date DATE not null,
+    StartTime TIME not null,
+    EndTime TIME CHECK ( EndTime > StartTime ),
+    TrainingTypeID INT not null,
     HallID INT,
-    TrainerID INT,
-    ManagerID INT,
+    Trainer INT,
+    Manager INT,
     FOREIGN KEY (TrainingTypeID) REFERENCES "TrainingType"(ID) ON DELETE CASCADE,
-    FOREIGN KEY (HallID) REFERENCES "Hall"(ID) ON DELETE CASCADE,
-    FOREIGN KEY (TrainerID) REFERENCES "Trainer"(TrainerID) ON DELETE CASCADE,
-    FOREIGN KEY (ManagerID) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
+    FOREIGN KEY (HallID) REFERENCES "Hall"(ID) ON DELETE SET NULL,
+    FOREIGN KEY (Trainer) REFERENCES "Trainer"(TrainerID) ON DELETE SET NULL,
+    FOREIGN KEY (Manager) REFERENCES "Manager"(ManagerID) ON DELETE SET NULL
 );
 
--- TrainingAttendance Table (References Training.ID, Client.ClientID)
 CREATE TABLE "TrainingAttendance" (
-    ID SERIAL PRIMARY KEY,
     TrainingID INT,
-    ClientClientID INT,
+    ClientID INT,
+    PRIMARY KEY (TrainingID, ClientID),
     FOREIGN KEY (TrainingID) REFERENCES "Training"(ID) ON DELETE CASCADE,
-    FOREIGN KEY (ClientClientID) REFERENCES "Client"(ClientID) ON DELETE CASCADE
+    FOREIGN KEY (ClientID) REFERENCES "Client"(ClientID) ON DELETE CASCADE
 );
