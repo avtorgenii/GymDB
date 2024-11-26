4e45c611031a455b2c4607f28a93fd2c59a7b3f1
ashes <alexandershestakov03@gmail.com>
2024-11-26 18:48:42 +0100

    feat: add new queries and indexes

--- sql/delete_indexes.sql
+++ sql/delete_indexes.sql
@@ -2,3 +2,6 @@ drop index if exists idx_locker_status;
 
 drop index if exists idx_fault_status_equipmentid;
 drop index if exists idx_equipment_name_status;
+
+drop INDEX if exists idx_availability_date;
+drop index if exists idx_user_email_hash;
